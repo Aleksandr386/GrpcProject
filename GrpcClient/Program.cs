2b21110e2919f385d2c3d263bb0c39c2b284d7cf
Alex B <abartosevic386@gmail.com>
2023-05-17 00:55:41 +0300

    Client

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace GrpcClient
+﻿using Grpc.Net.Client;
+using GrpcServer;
+
+namespace GrpcClient
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var channel = GrpcChannel.ForAddress("https://localhost:7128");
+            var client = new Greeter.GreeterClient(channel);
+
+            var input = new HelloRequest { Name = "Alex" };
+
+            var reply = await client.SayHelloAsync(input);
+
+            Console.WriteLine(reply.Message);
+
+            Console.ReadLine();
         }
     }
 }
