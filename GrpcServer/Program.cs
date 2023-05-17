c0260863a8b437665953c18abeb18e8f3aeaae9e
Alex B <abartosevic386@gmail.com>
2023-05-18 01:32:14 +0300

    Swagger

--- GrpcServer/Program.cs
+++ GrpcServer/Program.cs
@@ -1,4 +1,5 @@
 using GrpcServer.Services;
+using Microsoft.OpenApi.Models;
 
 namespace GrpcServer
 {
@@ -8,12 +9,26 @@ namespace GrpcServer
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            builder.Services.AddGrpc();
+            builder.Services.AddGrpc().AddJsonTranscoding();
+            builder.Services.AddGrpcSwagger();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",
+                    new OpenApiInfo { Title = "gRPC transcoding", Version = "v1" });
+                var filePath = Path.Combine(AppContext.BaseDirectory, "GrpcServer.xml");
+                c.IncludeXmlComments(filePath);
+                c.IncludeGrpcXmlComments(filePath, includeControllerXmlComments: true);
+            });
+
 
             var app = builder.Build();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.MapGrpcService<GreeterService>();
-            app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client");
 
             app.Run();
         }
